6632ee2a5e39d0e676033c37250b04100d91f02b
nourhan <nourhanadelhessin@gmail.com>
2024-10-04 02:22:29 +0300

    review part

---
+++ JS/main.js
@@ -0,0 +1,77 @@
+// STORAGE PRODUCTS----------------------------
+var storageProducts = [
+    {
+        id: 1,
+        name: "Anderson Chest Of Drawers, Mocha",
+        price: 99.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/18-800x534.jpg",
+        category: "Storage",
+    },
+
+    {
+        id: 2,
+        name: "Larsen Wide Chest Of Drawers",
+        price: 189.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/20-800x800.jpg",
+        category: "Storage",
+    },
+
+    {
+        id: 3,
+        name: "Lucien Bedside, Light Mango Wood",
+        price: 159.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/19-800x560.jpg",
+        category: "Storage",
+    },
+
+    {
+        id: 4,
+        name: "Lucien Wide Chest of Drawers",
+        price: 299.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/21.jpg",
+        category: "Storage",
+    },
+]
+
+// LIGHTING PRODUCTS----------------------------
+var lightingProducts = [
+    {
+        id: 1,
+        name: "Albert Ceiling Spotlight Bar, Charcoal",
+        price: 289.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/28-800x652.jpg",
+        category: "Lighting",
+    },
+
+    {
+        id: 2,
+        name: "Chicago Large Pendant, Green & Brass",
+        price: 129.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/30-800x800.jpg",
+        category: "Lighting",
+    },
+
+    {
+        id: 3,
+        name: "Gigi Tilting Pendant Chandelier Light",
+        price: 89.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/1-13.jpg",
+        category: "Lighting",
+    },
+
+    {
+        id: 4,
+        name: "Java Shade Small, Natural Rattan",
+        price: 189.00,
+        description: "Proactively communicate corporate process improvements via corporate scenarios. Progressively aggregate proactive data after diverse users. Rapidiously redefine front-end interfaces before go forward process improvements.",
+        imageUrl: "https://demos.reytheme.com/beijing/wp-content/uploads/sites/7/2019/06/1-12.jpg",
+        category: "Lighting",
+    },
+]
